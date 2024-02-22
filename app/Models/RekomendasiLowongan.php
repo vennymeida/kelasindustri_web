1e8271d47f66c6ccce313f9be6c98439bacf6b89
vennymeida <vnymeida@gmail.com>
2024-02-22 14:28:24 +0700

    model rekomendasi

--- app/Models/RekomendasiLowongan.php
+++ app/Models/RekomendasiLowongan.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Keahlian;
 
 class RekomendasiLowongan extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'lulusan_id',
+        'loker_id',
+    ];
 }
